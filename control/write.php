1767cef0fafbdede27536a73becd210537096bbc
rtblgst <rtblgst@gmail.com>
2017-03-14 20:15:10 +0900

    Thumbnail is displayed at top left on screen.

--- control/write.php
+++ control/write.php
@@ -30,22 +30,18 @@
 
 <!-- <form class="" action="write_process.php" method="post"> -->
 <form class="" name="whatForm">
-  <div class="form-group">
-    <label for="form-title">제목:</label>
-    <input type="text" class="form-control" name="title" id="form-title" placeholder="Type title here">
+	<img id="launcher_icon_img" src=".\control/Pavicon512x512.png" width="75" height="75" alt="xXx"/>
+	<label id="id_float_center" for="form-loginID">작성자: <?php echo $loginID?></label>
+	
+  <div>
+    <!-- <label for="form-title">제목:</label> -->
+		<input class="form-control" type="text" name="title" id="form-title" placeholder="앱 타이틀을 여기에.">
   </div>
 
-  <div class="form-group">
-    <label for="form-loginID">작성자: <?php echo $loginID?></label>
-    <!-- <input type="text" class="form-control" name="author" id="form-author" placeholder="Type author here" value=<?php //echo $loginID?>> -->
-		<input type="hidden" class="form-control" name="loginID" id="form-loginID" value=<?php echo $loginID?>>
+  <div>
+    <!-- <label for="form-description">본문:</label> -->
+    <textarea class="form-control" name="description" id="form-description" rows="10" placeholder="앱 설명을 적으세요"></textarea>
   </div>
-
-  <div class="form-group">
-    <label for="form-description">본문:</label>
-    <textarea class="form-control" name="description" id="form-description" rows="10" placeholder="Type description here"></textarea>
-  </div>
-
 
   <input type="hidden" role="uploadcare-uploader" />
   <!-- <input type="submit" value="쓰기 완료" name="name" class="btn btn-success"> -->
@@ -61,10 +57,8 @@
 		<i class="glyphicon glyphicon-plus"></i>
 		<span>Select files...</span>
 		<!-- The file input field used as target for the file upload widget -->
-		<input id="fileupload" type="file" name="files[]" multiple>
+		<input id="fileupload" type="file" name="files[]" >	<!-- '', multiple -->
   </span>
-  <br>
-  <br>
   <!-- The global progress bar -->
   <div id="progress" class="progress">
 		<div class="progress-bar progress-bar-success"></div>
@@ -99,14 +93,41 @@ $(function () {
         url: url,
         dataType: 'json',
         done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo('#files');
-            });
+             $.each(data.result.files, function (index, file) {
+								// var sTmp = ".\\jQuery-File-Upload\\server\\php\\files\\" +file.name;	//oOo. require Double-rSlash(//).
+								// var sTmp = './jQuery-File-Upload/server/php/files/' +file.name;		//oOo
+								// var sTmp = '.\jQuery-File-Upload\server\\php\files\\' +file.name;	// xXx
+								var sTmp = './jQuery-File-Upload/server/php/files/thumbnail/' +file.name;		//oOo
+								// $('<p/>').text(sTmp).appendTo('#files');
+								$("#launcher_icon_img").attr("src", sTmp);
+								// alert(sTmp);
+								// $('yourimageselector').attr('src', 'newsrc').load(function(){
+										// this.width;   // Note: $(this).width() will not work for in memory images
+								// });
+								// http://stackoverflow.com/questions/554273/changing-the-image-source-using-jquery
+								// http://plaboratory.org/archives/2999
+             });
         },
         progressall: function (e, data) {
             var progress = parseInt(data.loaded / data.total * 100, 10);
             $('#progress .progress-bar').css('width', progress + '%');
         }
+    }).on('fileuploaddone', function (e, data) {
+        // $.each(data.result.files, function (index, file) {
+            // if (file.url) {
+                // var link = $('<a>')
+                    // .attr('target', '_blank')
+                    // .prop('href', file.url);
+                // $(data.context.children()[index])
+                    // .wrap(link);
+            // } else if (file.error) {
+                // var error = $('<span class="text-danger"/>').text(file.error);
+                // $(data.context.children()[index])
+                    // .append('<br>')
+                    // .append(error);
+            // }
+        // });
+				// alert("Done!");
     }).prop('disabled', !$.support.fileInput)
         .parent().addClass($.support.fileInput ? undefined : 'disabled');
 });
--- part/nav.php
+++ part/nav.php
@@ -21,7 +21,7 @@ $(function(){
 	$("#tstResult").click(function(){
 		$.ajax({
 			type: 'GET',
-			url: 'part/nav_test.php?test=[- indent2 -]',
+			url: 'part/nav_test.php?test=[- m -]',
 			dataType : 'text',
 			error : function() {
 			  alert('Fail!!');
